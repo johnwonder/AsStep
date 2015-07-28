f1c6b21ed717514323861327080573165b61fcab
johnwonder <johnufida@163.com>
2015-07-29 02:10:16 +0800

    FlatUI Test

--- databasetest/src/main/java/com/example/john/databasetest/MainActivity.java
+++ databasetest/src/main/java/com/example/john/databasetest/MainActivity.java
@@ -1,21 +1,46 @@
 package com.example.john.databasetest;
 
+import android.app.ActionBar;
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.cengalabs.flatui.FlatUI;
+
 
 public class MainActivity extends Activity {
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+       // FlatUI.setDefaultTheme(FlatUI.ORANGE);
+    }
+
     private  MyDatabaseHelper dbHelper;
+
+    public MainActivity() {
+
+        super();
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FlatUI.setDefaultTheme(FlatUI.ORANGE);
+
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        FlatUI.setDefaultTheme(FlatUI.ORANGE);
+        //FlatUI.setDefaultTheme(R.array.blood);
+        //ActionBar actionBar = getActionBar();
+        //actionBar.show();
+        getActionBar().setBackgroundDrawable(FlatUI.getActionBarDrawable(this,FlatUI.ORANGE, false));
         dbHelper = new MyDatabaseHelper(this,"BookStore.db",null,1);
         Button createDatabase = (Button)findViewById(R.id.create_database);
         createDatabase.setOnClickListener(new View.OnClickListener() {
