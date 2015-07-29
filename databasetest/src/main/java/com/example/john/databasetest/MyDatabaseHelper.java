02081c1445f77f6b15d7f954c76a51beec21d4cd
johnwonder <johnufida@163.com>
2015-07-30 00:24:08 +0800

    sqlite database crud

--- databasetest/src/main/java/com/example/john/databasetest/MyDatabaseHelper.java
+++ databasetest/src/main/java/com/example/john/databasetest/MyDatabaseHelper.java
@@ -17,6 +17,13 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
             +"pages integer,"
             +"name text)";
 
+    public  static  final String CREATE_CATEGORY = "create table Category ("
+            +"id integer primary key autoincrement, "
+            +"category_name text,"
+            +"category_code integer)";
+
+
+
     private Context mContext;
 
     public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
@@ -32,6 +39,8 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+            db.execSQL("drop table if exists Book");
+            db.execSQL("drop table if exists Category");
+            onCreate(db);
     }
 }
